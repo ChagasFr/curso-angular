5c944067fa1c745492a82b1bfa5601c313baae9c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2023-11-28 18:46:47 -0300

    feat: list-render

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Animal } from '../../Animal';
 
 @Component({
@@ -7,14 +8,20 @@ import { Animal } from '../../Animal';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Turca', type: 'Dog' },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Frida', type: 'Dog' },
-    { name: 'Bob', type: 'Horse' },
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 4 },
+    { name: 'Tom', type: 'Cat', age: 10 },
+    { name: 'Frida', type: 'Dog', age: 11 },
+    { name: 'Bob', type: 'Horse', age: 2 },
   ];
+
+  animalDetails = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos de vida!`;
+  }
 }
